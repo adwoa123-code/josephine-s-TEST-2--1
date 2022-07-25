d830a98b23ff8ff2b8a0d5a6ee5397920e216827
Josephine Obuobia Darko <66028864+adwoa123-code@users.noreply.github.com>
2022-07-25 21:34:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Below is the code needed for the Fortune to be told.
+import random
+name = input("What is your name?: ")
+def Your_Fortune() :
+  options = ('rich in five years time','You will marry next year ','You will win the presidential elections')
+  result = random.choice(options)
+  print (result)
+Your_Fortune()
+Your_Fortune()
+Your_Fortune()
+
+  
+
+
+
+
+   
+
+                                                                                                        
